175079645e080a658ed12e0b44e1024565dd962a
Fariq Irham Hudiya <fariqhudiya49@gmail.com>
2022-10-24 10:55:24 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Fariq";
+$usia = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $usia tahun.<br>";
+
+$namaAdik = "Fahmi";
+$usiaAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia
+$usiaAdik tahun.<br>";
+
+$selisihusia = $usia - $usiaAdik;
+echo "selisih usia saya dan adik saya adalah $selisihusia tahun.<br>";
+
+?>
